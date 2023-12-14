522cab8b54ad7ddf84e701b4955f2354cd773a05
redfoxofwealden <>
2023-12-14 17:05:18 +0000

    add row sorting to about.html

--- run.py
+++ run.py
@@ -1,23 +1,33 @@
 import os
+import json
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
-    return render_template('about.html')
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
-@app.route('/contact')
+
+@app.route("/contact")
 def contact():
-    return render_template('contact.html')
+    return render_template("contact.html", page_title="Contact")
 
-@app.route('/careers')
+
+@app.route("/careers")
 def careers():
-    return render_template('careers.html')
+    return render_template("careers.html", page_title="Careers")
+
 
 if __name__ == '__main__':
     app.run(
